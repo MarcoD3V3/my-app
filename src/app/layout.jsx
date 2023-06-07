e0c177664bc281f24c4829938df46e5929a49cb6
MarcoD3V3 <git.sinister.developer@gmail.com>
2023-06-07 06:07:26 -0500

    segun yo ya esta bien

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,28 +1,32 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
 'use client'
 import '../app/globals.css'
 import { Inter } from 'next/font/google'
-import {Footer} from '../components/Footer/index'
-import {NavBar} from '../components/NavBar/index'
+import { Footer } from '../components/Footer/index'
+import { NavBar } from '../components/NavBar/index'
 import { ChakraProvider } from '@chakra-ui/react'
-import { CacheProvider  } from '@chakra-ui/next-js'
+import { CacheProvider } from '@chakra-ui/next-js'
 
+const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'Home | Page',
+  description: 'Esta es una pagina personal que la utilizo para hacer pruevas de mis habilidades de desarrollador web y hacer ver mi progreso.'
+}
 
 export default function RootLayout ({ children }) {
   return (
     <html lang="es">
-      <body>
-        <NavBar/>
-          <div className='main__children__body'>
-            <CacheProvider>
-              <ChakraProvider>
-                {children}
-              </ChakraProvider>
-            </CacheProvider>
-          </div> 
+      <body className={inter.className}>
+        <NavBar />
+        <div className="main__children__body">
+          <CacheProvider>
+            <ChakraProvider>{children}</ChakraProvider>
+          </CacheProvider>
+        </div>
         <Footer />
       </body>
     </html>
   )
 }
-
